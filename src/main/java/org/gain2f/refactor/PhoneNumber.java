c9d8a944a1aed0c22900be118fa05761ecef3465
Aleksey Kiselyov <voilesik@gmail.com>
2016-02-11 09:45:49 +0200

    initial commit

---
+++ src/main/java/org/gain2f/refactor/PhoneNumber.java
@@ -0,0 +1,14 @@
+package org.gain2f.refactor;
+
+public class PhoneNumber {
+	private Num number;
+
+	public PhoneNumber(String number) {
+		this.number = new Num(number);
+	}
+
+	public String getNumber() {
+		return number.getNumber();
+	}
+	
+}
